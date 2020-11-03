57075e7cc277da20a047396b2be5540a5a88a91a
Rahul prasad <prasadrahul9719@gmail.com>
2020-11-03 14:44:40 +0530

    9th commit added flash image ........to be continued .....

--- app/src/main/java/com/example/design/menudriver.java
+++ app/src/main/java/com/example/design/menudriver.java
@@ -3,6 +3,8 @@ package com.example.design;
 import android.app.AppComponentFactory;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ToggleButton;
 import android.widget.Toolbar;
 
@@ -13,8 +15,6 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 public class menudriver extends AppCompatActivity {
 
-private DrawerLayout mDrawerLayout;
-private ActionBarDrawerToggle mToggle;
 
 
 
@@ -22,28 +22,11 @@ private ActionBarDrawerToggle mToggle;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menudriver);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
-        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
-        mDrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
 
 
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(mToggle.onOptionsItemSelected(item)){
-            return true;
-        }
 
-
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    //
 }
