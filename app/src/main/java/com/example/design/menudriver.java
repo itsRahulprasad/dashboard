d62d25dac4f4949dd06994b16606a3fd56f7db94
Rahul prasad <prasadrahul9719@gmail.com>
2020-11-03 10:43:28 +0530

    7th commit added toggle bar but not working hahahahahahahaha

--- app/src/main/java/com/example/design/menudriver.java
+++ app/src/main/java/com/example/design/menudriver.java
@@ -2,16 +2,19 @@ package com.example.design;
 
 import android.app.AppComponentFactory;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ToggleButton;
 import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 public class menudriver extends AppCompatActivity {
 
-
+private DrawerLayout mDrawerLayout;
+private ActionBarDrawerToggle mToggle;
 
 
 
@@ -19,14 +22,28 @@ public class menudriver extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menudriver);
-
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
 
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
 
 
-//
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    //
 }
